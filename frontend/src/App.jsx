111ba1ca589219281823e9fb4930edbf7fa8ff42
Saif Al-saedi <saif4004@gmail.com>
2024-10-30 13:44:38 -0700

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,30 @@
+// frontend/src/App.jsx
 import React from 'react';
 
-import PhotoListItem from './components/PhotoListItem';
-import './App.scss';
+ import PhotoListItem from './components/PhotoListItem';
+ import PhotoList from './components/PhotoList';
+ import './App.scss';
 
+const sampleDataForPhotoListItem = {
+  id: "1",
+  location: {
+    city: "Montreal",
+    country: "Canada",
+  },
+  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
+  username: "Joe Example",
+  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
+};
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
-    <div className="App">
-      <PhotoListItem/>
-    </div>
+    <PhotoListItem
+    imageSource={sampleDataForPhotoListItem.imageSource}
+    profile={sampleDataForPhotoListItem.profile}
+    username={sampleDataForPhotoListItem.username}
+    location={sampleDataForPhotoListItem.location}
+    />
+    
   );
 };
 
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -14,8 +14,18 @@ const sampleDataForPhotoListItem = {
   profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
 };
 
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  console.log(props);
+
+  return (
+    <div>
+      <img src={props.imageSource} alt="Photo" />
+      <br />
+      <img src={props.profile} alt="Profile" />
+      <h1>{props.username}</h1>
+      <h1>{props.location.city} {props.location.country}</h1>
+    </div>    
+  );
 };
 
 export default PhotoListItem;
