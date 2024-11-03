16b27494224434093e6f02bd25d645fb0289537a
Saif Al-saedi <saif4004@gmail.com>
2024-11-03 10:37:49 -0800

    Fixed a bug where it wasnt display when a user fav a photo

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -1,14 +1,14 @@
+// frontend/src/components/FavBadge.jsx
 import React from 'react';
 import FavIcon from './FavIcon';
-
 import '../styles/FavBadge.scss';
 
 const FavBadge = ({ isFavPhotoExist }) => {
   return (
     <div className='fav-badge'>
-      <FavIcon displayAlert={!!isFavPhotoExist}/>
+      <FavIcon displayAlert={!!isFavPhotoExist} /> {/* Pass displayAlert based on isFavPhotoExist */}
     </div>
-  ) 
+  );
 };
 
 export default FavBadge;
