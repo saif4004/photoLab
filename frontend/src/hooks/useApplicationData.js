8d621089aa6db525e4241556933695aee97c5e43
Saif Al-saedi <saif4004@gmail.com>
2024-11-03 13:41:27 -0800

    Using reducer now

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,33 +1,101 @@
-// hooks/useApplicationData.js
-import { useState } from 'react';
+import { useReducer } from 'react';
+
+export const ACTIONS = {
+  FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
+  FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS',
+  CLOSE_MODAL: 'CLOSE_MODAL',
+};
+
+const initialState = {
+  favourites: [],
+  isModalOpen: false,
+  clickedPhoto: null,
+  photoData: null,
+  topicData: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case ACTIONS.FAV_PHOTO_ADDED:
+      return {
+        ...state,
+        favourites: [...state.favourites, action.photoId],  
+      };
+
+    case ACTIONS.FAV_PHOTO_REMOVED:
+      return {
+        ...state,
+        favourites: state.favourites.filter(id => id !== action.photoId),  
+      };
+
+    case ACTIONS.SET_PHOTO_DATA:
+      return {
+        ...state,
+        photoData: action.photoData,  
+      };
+
+    case ACTIONS.SET_TOPIC_DATA:
+      return {
+        ...state,
+        topicData: action.topicData,  
+      };
+
+    case ACTIONS.SELECT_PHOTO:
+      return {
+        ...state,
+        clickedPhoto: action.photo,  
+        isModalOpen: true,
+      };
+
+    case ACTIONS.DISPLAY_PHOTO_DETAILS:
+      return {
+        ...state,
+        isModalOpen: true,
+      };
+      case ACTIONS.CLOSE_MODAL: 
+      return {
+        ...state,
+        isModalOpen: false,
+        clickedPhoto: null,
+      };
+
+    default:
+      throw new Error(
+        `Tried to reduce with unsupported action type: ${action.type}`
+      );
+  }
+}
 
 const useApplicationData = () => {
-  const [favourites, setFavourites] = useState([]);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [clickedPhoto, setClickedPhoto] = useState(null);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   const toggleFavourite = (photoId) => {
-    if (favourites.includes(photoId)) {
-      setFavourites(favourites.filter((id) => id !== photoId));
+    if (state.favourites.includes(photoId)) {
+      dispatch({ type: ACTIONS.FAV_PHOTO_REMOVED, photoId });  
     } else {
-      setFavourites([...favourites, photoId]);
+      dispatch({ type: ACTIONS.FAV_PHOTO_ADDED, photoId });  
     }
   };
 
   const handlePhotoClick = (photo) => {
-    setClickedPhoto(photo);
-    setIsModalOpen(true);
+    dispatch({ type: ACTIONS.SELECT_PHOTO, photo });  
   };
 
   const closeModal = () => {
-    setIsModalOpen(false);
-    setClickedPhoto(null);
+    dispatch({ type: ACTIONS.CLOSE_MODAL }); 
   };
+  
 
   return {
-    favourites,
-    isModalOpen,
-    clickedPhoto,
+    favourites: state.favourites,
+    isModalOpen: state.isModalOpen,
+    clickedPhoto: state.clickedPhoto,
+    photoData: state.photoData,
+    topicData: state.topicData,
     toggleFavourite,
     handlePhotoClick,
     closeModal,
